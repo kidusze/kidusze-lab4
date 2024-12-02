8de6cc89c4be80c1b99f7749dd866b87bc058f62
Kidus Zerihun <kiduszerihun@Kiduss-MacBook-Pro.local>
2024-12-01 21:01:25 -0500

    Setup basic Enigma frame

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -1,3 +1,13 @@
-public class EnigmaFrame {
-    
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class EnigmaFrame extends JFrame {
+    public EnigmaFrame() {
+        // Set up the frame
+        setTitle("Enigma GUI");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 500); // Increase window size
+        setLayout(new BorderLayout());
+    }
 }
